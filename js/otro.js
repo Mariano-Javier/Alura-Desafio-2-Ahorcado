c99ec03666cb6e138d42c3d30527e45d79246576
Mariano-Javier <mariano.javier.choqui@gmail.com>
2022-06-05 01:04:42 -0300

    segundo commit

--- js/otro.js
+++ js/otro.js
@@ -9,3 +9,11 @@ tablero.addEventListener(
   false
 );
 
+let vectorTemp = [0,3,4]
+function visualizarLetra(vectorTemp) {
+  let letra = document.querySelectorAll(".letrasIndividuales");
+
+  for (let i=0;i<vectorTemp.length;i++){
+    letra[vectorTemp[i]].classList.remove("invisible")
+  }
+}
--- js/script.js
+++ js/script.js
@@ -33,6 +33,8 @@ function generadorPalabrasRandom() {
   pincel.clearRect(0, 0, tablero.width, tablero.height); //limpia el canvas
   document.getElementById('bloqueLetras').innerHTML = '';//limpia las letas del juego anterior
   document.getElementById('bloqueRenglones').innerHTML = '';//limpia los renglones del juego anterior
+  document.getElementById('letrasPresionadas').innerHTML = '';//limpia las letras presionadas del juego anterior.
+  
 
   //dibuja la base
   dibujarLineas(0, 357, 294, 358);
@@ -48,7 +50,7 @@ function generadorPalabrasRandom() {
   let pruebaVector = palabraDividida;
 for (i = 0; i < pruebaVector.length; i++) {
   let nuevoDivL = document.createElement("span");
-  nuevoDivL.classList.add("letrasIndividuales");
+  nuevoDivL.classList.add("letrasIndividuales","invisible");
   nuevoDivL.innerHTML = pruebaVector[i];
   bloqueLetras.appendChild(nuevoDivL);
 }
@@ -62,19 +64,25 @@ for (let i=0; i<pruebaVector.length; i++){
 }
 }
 
+let visorLetras = document.querySelector("#letrasPresionadas");
+
+
 //evento que escucha el teclado
 document.addEventListener("keydown", (event) => {
     let nombre = event.key;
-
-
     console.log("tecla presionada: " + nombre);
-    if (nombre.match(/^([a-z0-9|ñ|]{1,})$/)) {//[a-z0-9] limite de valores, {1,} indica el largo de caracteres, en este caso solo buscamos 1, ya que hay teclas q comienzan con caracteres validos.
+    if (nombre.match(/^([a-z|ñ|]{1,})$/)) {//[a-z0-9] limite de valores, {1,} indica el largo de caracteres, en este caso solo buscamos 1, ya que hay teclas q comienzan con caracteres validos.
       let indices = []; //vector que va a devolver los valores de los indices que el usuario ingreso por teclado
       let array = palabraABuscar; //vector con la palabra dividida
       let element = nombre; //caracter que ingresa el usuario por teclado
       let idx = array.indexOf(element);
 
       if (idx == -1) {
+        let nuevaLetraInvalida = document.createElement("div");
+        nuevaLetraInvalida.innerHTML = element;
+        nuevaLetraInvalida.classList.add("letrasInvalidas");
+        visorLetras.appendChild(nuevaLetraInvalida);
+
         console.log("valor no encontrado");
         contadorErrores++;
         dibujadorDePartes();
